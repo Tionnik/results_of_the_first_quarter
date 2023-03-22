c75e8e0a3c48646afbeedb767a79f4c14c88f51c
Nik <ice_fire50@mail.ru>
2023-03-22 15:13:32 +0300

    block diagram

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+Console.WriteLine("Введите текст для распознания");
+string text = Console.ReadLine()!;
+Console.WriteLine("Введите кол-во элементов для распознания");
+int n = int.Parse(Console.ReadLine()!);
